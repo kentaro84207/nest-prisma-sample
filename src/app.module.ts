d80a3dd7df44c7eb123660920e6d8fe875ff8b6f
Kentaro Suzuki <kentaro84207@gmail.com>
2022-01-30 17:57:53 +0900

    set graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { PrismaService } from './prisma.service';
+import { PostsResolver } from './posts/posts.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, PostsResolver],
 })
 export class AppModule {}
